85cc4edffd3ebf23c203dcedd6772c835488a6ec
wuyeelok <wuyeelok@gmail.com>
2024-04-13 07:59:02 +0800

    Show fixed add button in bottom of the page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecipeCardComponent],
+  imports: [RecipeCardComponent, MatIconModule, MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
