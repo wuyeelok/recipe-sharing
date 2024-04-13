c450a1504a426e20bb968dbeb0e0a07ac7c02a28
wuyeelok <wuyeelok@gmail.com>
2024-04-13 09:55:31 +0800

    Basic create form

--- src/app/pages/create-recipe-form/create-recipe-form.component.ts
+++ src/app/pages/create-recipe-form/create-recipe-form.component.ts
@@ -2,12 +2,31 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-create-recipe-form',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatRadioModule,
+  ],
   templateUrl: './create-recipe-form.component.html',
   styleUrl: './create-recipe-form.component.scss',
 })
-export class CreateRecipeFormComponent {}
+export class CreateRecipeFormComponent {
+  recipeItem: any = {
+    title: '',
+    description: '',
+    foodType: '',
+    image: '',
+  };
+
+  onSubmit() {
+    console.log('values', this.recipeItem);
+  }
+}
