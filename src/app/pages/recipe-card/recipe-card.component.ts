42fff45bd6e5b0f939cab2dad4fa30c69bddb7ec
wuyeelok <wuyeelok@gmail.com>
2024-04-13 10:04:56 +0800

    Card icon

--- src/app/pages/recipe-card/recipe-card.component.ts
+++ src/app/pages/recipe-card/recipe-card.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss',
 })
