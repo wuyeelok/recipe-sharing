05f5e975ef010067764bb107336206ca72e1d615
wuyeelok <wuyeelok@gmail.com>
2024-04-13 08:56:25 +0800

    Start working on create recipe form

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateRecipeFormComponent } from '../create-recipe-form/create-recipe-form.component';
 
 @Component({
   selector: 'app-home-page',
@@ -12,4 +14,10 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class HomePageComponent {
   recipes = [1, 1, 1, 1, 11, 1];
+
+  constructor(public dialog: MatDialog) {}
+
+  handleOpenCreateRecuoeForm() {
+    this.dialog.open(CreateRecipeFormComponent);
+  }
 }
