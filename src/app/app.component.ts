542ed29df34016848f07a881654cd4e8f814e8ac
wuyeelok <wuyeelok@gmail.com>
2024-04-15 22:15:49 +0800

    Auth page: Start working on component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomePageComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent,
+    AuthComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
